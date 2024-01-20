4b27a17045c24b23439ed4e691acca19d6fce435
Pmiay <83475095+Pmiay@users.noreply.github.com>
2024-01-20 10:04:03 -0500

    Add files via upload

--- scripts/miyuro.script.bcode.js
+++ scripts/miyuro.script.bcode.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+	let docContent = [];
+    const DOMlista = document.querySelector('#lista');
 	const $resultados = document.querySelector("#resultado");
+    const miLocalStorage = window.localStorage;
+
 	Quagga.init({
 		inputStream: {
 			constraints: {
@@ -18,13 +22,54 @@ document.addEventListener("DOMContentLoaded", () => {
 			console.log(err);
 			return
 		}
-		console.log("Iniciado correctamente");
+		console.log("En Funcionamiento");
 		Quagga.start();
 	});
 
 	Quagga.onDetected((data) => {
-		$resultados.textContent = data.codeResult.code;
+		var code = data.codeResult.code;
+		$resultados.textContent = code; //data.codeResult.code;
+				
+		savereader (code);
+		beep();
 		// Imprimimos todo el data para que puedas depurar
 		console.log(data);
 	});
+
+function beep() {
+	var bip = new Audio('./assets/sound/bipOne.mp3');
+	bip.play();
+}
+
+function savereader(code){  // https://www.w3schools.com/js/tryit.asp?filename=tryjs_json_parse
+	var qty = document.getElementById("qty").value;
+/* 	let docContent = '{"lecturas":[' +
+	'{"bcode":"' + code + '","qty":' + qty + ',"now":"dd-mm-yy 00:00" },' +
+	'{"bcode":"' + code + '",qty:' + qty + ',"now":"dd-mm-yy 00:00" }'; */
+	docContent.push( code); //'{"bcode":"' + code + '",qty:' + qty + ',"now":"dd-mm-yy 00:00" }');
+
+	const lectdata =  JSON.stringify(docContent);
+	document.getElementById("lecturas").innerHTML = lectdata; 
+}
+function ToStorage () { 
+	miLocalStorage.setItem('lecturas', JSON.stringify(docContent));
+}
+
+function FromStorage () {
+	// ¿Existe un carrito previo guardado en LocalStorage? 
+	if (miLocalStorage.getItem('lecturas') !== null) {  // Carga la información
+		docContent = JSON.parse(miLocalStorage.getItem('lecturas'));
+	}
+}
+	//starting
+	FromStorage()
+
+
 });
+/* let docContent = '{"lecturas":[' +
+	'{"bcode":"00000","qty":qty,"now":"dd-mm-yy 00:00" },' */
+
+
+function add(){
+	text. document.getElementById("demo").value
+}
